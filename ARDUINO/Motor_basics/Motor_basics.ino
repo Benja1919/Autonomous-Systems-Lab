b249c185789849a3f5585a780737006f5f41e13c
oriavrahami <92465232+oriavrahami@users.noreply.github.com>
2024-02-02 09:04:10 +0200

    Add files via upload

--- ARDUINO/Motor_basics/Motor_basics.ino
+++ ARDUINO/Motor_basics/Motor_basics.ino
@@ -1,22 +1,25 @@
 int in1 = 5;
 int in2 = 6;
 void setup() {
-  // put your setup code here, to run once:
+  // Set the 'output' pins
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Spin CW for 5 seconds
   digitalWrite (in1, HIGH);
   digitalWrite (in2, LOW);
   delay(5000);
+  //Break for 5 seconds
   digitalWrite (in1, HIGH);
   digitalWrite (in2, HIGH);
   delay(5000);
+  //spin CCW for 5 seconds
   digitalWrite (in1, LOW);
   digitalWrite (in2, HIGH);
   delay(5000);
-  digitalWrite (in1, HIGH); //break if needed
+  //Break again for 5 seconds (if needed)
+  digitalWrite (in1, HIGH);
   digitalWrite (in2, HIGH);
   delay(5000);
 }
