20512b09e6e94d6158bb6dda14ca39a8221eebbb
oriavrahami <92465232+oriavrahami@users.noreply.github.com>
2024-02-02 09:20:47 +0200

    Add files via upload

--- ARDUINO/motor_by_potentiometer/motor_by_potentiometer.ino
+++ ARDUINO/motor_by_potentiometer/motor_by_potentiometer.ino
@@ -1,34 +1,34 @@
 int in1 = 5;
 int in2 = 6;
 int speed = 127;
-int led = 9;         // the PWM pin the LED is attached to
+int led = 9;         // The PWM pin the LED is attached to
 int brightness = 0; 
 void setup() {
-  // put your setup code here, to run once:
+  // Set the outputs
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
     pinMode(led, OUTPUT);
     Serial.begin(9600);
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  int pos = analogRead(A0);
-  if (pos == 512){
+
+  int pos = analogRead(A0); //Get the position from potentiometer
+  if (pos == 512){ //Break
     analogWrite(led, LOW);
     analogWrite(in1, HIGH);
     analogWrite(in2 , HIGH);
     Serial.println(pos);
   }
-  if (pos > 512){
-    speed = map(pos, 512, 1023, 127, 255);
-    analogWrite(led, map(speed, 127, 255, 0, 255));
+  if (pos > 512){ //Spin to the right
+    speed = map(pos, 512, 1023, 127, 255); //Calc the speed of the spin in terms of in1/2 (value between 0-255)
+    analogWrite(led, map(speed, 127, 255, 0, 255)); // Set the LEDs power
     analogWrite(in1, speed);
     analogWrite(in2 , 255 - speed);
     Serial.println(speed);
   }
-  if(pos <512){
-    speed = map(pos, 0, 511, 255, 127);
-    analogWrite(led, map(speed, 127, 255, 0, 255));
+  if(pos <512){ // Spin to the lef
+    speed = map(pos, 0, 511, 255, 127); //Calc the speed of the spin in terms of in1/2 (value between 0-255)
+    analogWrite(led, map(speed, 127, 255, 0, 255)); // Set the LEDs power
     analogWrite(in1, 255-speed);
     analogWrite(in2 , speed);
     Serial.println(speed);
